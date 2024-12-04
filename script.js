cf8a82f4bc30e600353396cf3a532b828979c588
shweta8814 <krishna1119850@gmail.com>
2024-12-04 20:26:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your JS code here. If required.
 https://www.svgrepo.com/show/345221/three-dots.svg
+() => {
+  // Trigger the modal to open
+  cy.get('button').first().click().then(() => {
+    // Assert the modal is visible
+    cy.get('.modal').should('be.visible');
+    
+    // Click the close button
+    cy.get('.btn-close').click().then(() => {
+      // Wait for Bootstrap's fade-out animation
+      cy.wait(500); // Adjust the wait time if necessary
+      
+      // Assert the modal is no longer visible
+      cy.get('.modal').should('not.exist');
+    });
+  });
+};
